630647f34c61e9fd8e38c9c26640255403b40b93
unknown <tobiburrohman2@gmail.com>
2023-02-06 16:21:51 +0600

    first

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useEffect } from 'react'
+import Head from 'next/head'
+// import Custom Components
+import Header from '../landing/header'
+import GiftSection from '../landing/git-section'
+import DemoSection from '../landing/demo'
+import PortfolioSection from '../landing/portfolio'
+import WhatinsideSection from '../landing/whatinside'
+import BlogSection from '../landing/blog'
+import ElementSection from '../landing/elements'
+import FeaturesSection from '../landing/features'
+import TestimonialSection from '../landing/testimonial'
+import RatSection from '../landing/rat'
+import FooterSection from '../landing/footer'
+import PortfolioLayout from './layouts/portfolioLayouts'
+
+const Home = () => {
+  
+  useEffect(() => {
+    document.body.style.setProperty('--primary', '#10266b')
+    document.body.style.setProperty('--secondary', '#464545')
+    document.body.style.setProperty('--light', '#1F357D')
+    document.body.style.setProperty('--dark', '#04185B')
+  })
+
+  
+
+  return (
+    <Fragment>
+      
+
+      <PortfolioLayout/>
+
+      {/* <GiftSection />
+
+      <DemoSection />
+
+      <PortfolioSection />
+
+      <WhatinsideSection />
+
+      <BlogSection />
+
+      <ElementSection />
+
+      <FeaturesSection />
+
+      <TestimonialSection />
+
+      <RatSection /> */}
+
+    </Fragment>
+  )
+}
+
+export default Home
